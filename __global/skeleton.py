1a1def17ba5595f5a8a72829ea74b23f75b7303d
sahar-technion <sahar.sagy@campus.technion.ac.il>
2023-08-23 23:39:26 +0300

    added skeleton predictor

--- __global/skeleton.py
+++ __global/skeleton.py
@@ -9,19 +9,49 @@ class Skeleton:
 
     def __init__(
             self,
-            right_shoulder: np.ndarray,
-            left_shoulder: np.ndarray,
-            right_elbow: np.ndarray,
-            left_elbow: np.ndarray,
-            right_wrist: np.ndarray,
-            left_wrist: np.ndarray,
-            right_hip: np.ndarray,
-            left_hip: np.ndarray,
-            right_knee: np.ndarray,
-            left_knee: np.ndarray,
-            right_ankle: np.ndarray,
-            left_ankle: np.ndarray,
-            data
+            pelvis: np.ndarray = None,
+            left_hip: np.ndarray = None,
+            right_hip: np.ndarray = None,
+            belly: np.ndarray = None,
+            left_knee: np.ndarray = None,
+            right_knee: np.ndarray = None,
+            spine: np.ndarray = None,
+            left_ankle: np.ndarray = None,
+            right_ankle: np.ndarray = None,
+            thorax: np.ndarray = None,
+            left_toe: np.ndarray = None,
+            right_toe: np.ndarray = None,
+            neck: np.ndarray = None,
+            left_claw: np.ndarray = None,
+            right_claw: np.ndarray = None,
+            head: np.ndarray = None,
+            left_shoulder: np.ndarray = None,
+            right_shoulder: np.ndarray = None,
+            left_elbow: np.ndarray = None,
+            right_elbow: np.ndarray = None,
+            left_wrist: np.ndarray = None,
+            right_wrist: np.ndarray = None,
+            left_hand: np.ndarray = None,
+            right_hand: np.ndarray = None,
+            nose: np.ndarray = None,
+            left_eye: np.ndarray = None,
+            left_ear: np.ndarray = None,
+            right_eye: np.ndarray = None,
+            right_ear: np.ndarray = None,
+
+            # right_shoulder: np.ndarray,
+            # left_shoulder: np.ndarray,
+            # right_elbow: np.ndarray,
+            # left_elbow: np.ndarray,
+            # right_wrist: np.ndarray,
+            # left_wrist: np.ndarray,
+            # right_hip: np.ndarray,
+            # left_hip: np.ndarray,
+            # right_knee: np.ndarray,
+            # left_knee: np.ndarray,
+            # right_ankle: np.ndarray,
+            # left_ankle: np.ndarray,
+            data = None
     ):
         """
         Initializes a FrameRepresentation instance.
@@ -42,20 +72,51 @@ class Skeleton:
 
             data: The original data that created the frame
         """
-        self.right_shoulder = right_shoulder
-        self.left_shoulder = left_shoulder
-        self.right_elbow = right_elbow
-        self.left_elbow = left_elbow
-        self.right_wrist = right_wrist
-        self.left_wrist = left_wrist
-        self.right_hip = right_hip
-        self.left_hip = left_hip
-        self.right_knee = right_knee
-        self.left_knee = left_knee
-        self.right_ankle = right_ankle
-        self.left_ankle = left_ankle
 
+        self.pelvis = pelvis
+        self.left_hip = left_hip
+        self.right_hip = right_hip
+        self.belly = belly
+        self.left_knee = left_knee
+        self.right_knee = right_knee
+        self.spine = spine
+        self.left_ankle = left_ankle
+        self.right_ankle = right_ankle
+        self.thorax = thorax
+        self.left_toe = left_toe
+        self.right_toe = right_toe
+        self.neck = neck
+        self.left_claw = left_claw
+        self.right_claw = right_claw
+        self.head = head
+        self.left_shoulder = left_shoulder
+        self.right_shoulder = right_shoulder
+        self.left_elbow = left_elbow
+        self.right_elbow = right_elbow
+        self.left_wrist = left_wrist
+        self.right_wrist = right_wrist
+        self.left_hand = left_hand
+        self.right_hand = right_hand
+        self.nose = nose
+        self.left_eye = left_eye
+        self.left_ear = left_ear
+        self.right_eye = right_eye
+        self.right_ear = right_ear
         self.data = data
+        # self.right_shoulder = right_shoulder
+        # self.left_shoulder = left_shoulder
+        # self.right_elbow = right_elbow
+        # self.left_elbow = left_elbow
+        # self.right_wrist = right_wrist
+        # self.left_wrist = left_wrist
+        # self.right_hip = right_hip
+        # self.left_hip = left_hip
+        # self.right_knee = right_knee
+        # self.left_knee = left_knee
+        # self.right_ankle = right_ankle
+        # self.left_ankle = left_ankle
+
+        # self.data = data
 
     @classmethod
     def from_json(cls, json_str):
--- _process_data/predict_skeleton/model1/predict_skeleton.py
+++ _process_data/predict_skeleton/model1/predict_skeleton.py
@@ -1,16 +1,50 @@
 import tensorflow_hub as hub
 from __global.skeleton import Skeleton
+import tensorflow as tf
 
 model = hub.load('https://bit.ly/metrabs_l')  # or _s
-print(model.joint_names)
+NAME_DICT = {'pelv': 'pelvis', 'lhip': 'left_hip', 'rhip': 'right_hip', 'bell': 'belly', 'lkne': 'left_knee', 'rkne': 'right_knee', 'spin': 'spine', 'lank': 'left_ankle', 'rank': 'right_ankle', 'thor': 'thorax', 'ltoe': 'left_toe', 'rtoe': 'right_toe', 'neck': 'neck', 'lcla': 'left_claw', 'rcla': 'right_claw', 'head': 'head', 'lsho': 'left_shoulder', 'rsho': 'right_shoulder', 'lelb': 'left_elbow', 'relb': 'right_elbow', 'lwri': 'left_wrist', 'rwri': 'right_wrist', 'lhan': 'left_hand', 'rhan': 'right_hand', 'nose': 'nose', 'leye': 'left_eye', 'lear': 'left_ear', 'reye': 'right_eye', 'rear': 'right_ear', 'rfoo': 'right_foot', 'lfoo': 'left_foot', 'htop': 'head_top', 'lthu': 'left_thumb', 'rthu': 'right_thumb', 'lfin': 'left_finger', 'rfin': 'right_finger'}
 
 
-def model1_predict_skeleton(frame):
-    prediction = model.detect_poses(frame, skeleton='smpl+head_30')
-    # todo: need to convert "predicted_skeleton" to match the Skeleton class parameters
-    points = prediction['poses3d'][0, :, :]  # num_of_points, 3
-    # Create a Skeleton object using attributes_array
-    skeleton = None  # Skeleton(*attributes_array)
+def model1_predict_skeleton(frame, skeleton='smpl+head_30'):
+    tf_frame = tf.image.decode_image(tf.io.read_file(frame))
+    prediction = model.detect_poses(tf_frame, skeleton=skeleton) # we assume only one person in frame
+    coords = prediction['poses3d'][0].numpy()
+    coord_dict = {}
+    for i, joint_name in enumerate(model.per_skeleton_joint_names[skeleton].numpy().astype(str)):
+        coord_dict[NAME_DICT[joint_name[:4]]] = coords[0, i]
+    skeleton = Skeleton(
+        pelvis=coord_dict['pelvis'],
+        left_hip=coord_dict['left_hip'],
+        right_hip=coord_dict['right_hip'],
+        belly=coord_dict['belly'],
+        left_knee=coord_dict['left_knee'],
+        right_knee=coord_dict['right_knee'],
+        spine=coord_dict['spine'],
+        left_ankle=coord_dict['left_ankle'],
+        right_ankle=coord_dict['right_ankle'],
+        thorax=coord_dict['thorax'],
+        left_toe=coord_dict['left_toe'],
+        right_toe=coord_dict['right_toe'],
+        neck=coord_dict['neck'],
+        left_claw=coord_dict['left_claw'],
+        right_claw=coord_dict['right_claw'],
+        head=coord_dict['head'],
+        left_shoulder=coord_dict['left_shoulder'],
+        right_shoulder=coord_dict['right_shoulder'],
+        left_elbow=coord_dict['left_elbow'],
+        right_elbow=coord_dict['right_elbow'],
+        left_wrist=coord_dict['left_wrist'],
+        right_wrist=coord_dict['right_wrist'],
+        left_hand=coord_dict['left_hand'],
+        right_hand=coord_dict['right_hand'],
+        nose=coord_dict['nose'],
+        left_eye=coord_dict['left_eye'],
+        left_ear=coord_dict['left_ear'],
+        right_eye=coord_dict['right_eye'],
+        right_ear=coord_dict['right_ear'],
+        data=prediction
+        )
     return skeleton
 
 
