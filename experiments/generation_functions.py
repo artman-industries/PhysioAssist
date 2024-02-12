a89ee6e0293cdb83d81ff6e1465a1ab57f95664b
artman-industries <artman.industries2020@gmail.com>
2024-02-12 16:24:19 +0200

    .

--- experiments/generation_functions.py
+++ experiments/generation_functions.py
@@ -3,35 +3,27 @@ import math
 import numpy as np
 
 
-def test_15(s):
-    l = [1] * 7
-    return np.array([l] * 15, dtype=float)
-
-
-def _deterministic_model_generation_function(math_function, amount_of_frames):
+def _deterministic_model_abstract_generation_function(knee_angle_function, body_angle_function, amount_of_frames):
     data_list = []
 
     for i in range(amount_of_frames):
         x = float(i) / amount_of_frames
         # y = 1 - (x - x ** 2)
-        y = math_function(x)
+        y_knee = knee_angle_function(x)
+        y_body = body_angle_function(x)
 
         # Create a tensor
-        right_knee_angle = y
-        left_knee_angle = y
-        left_side_body_angle = 1
-        right_side_body_angle = 1
-
-        # Assign fixed values for the last three entries
-        entry_5 = 5
-        entry_6 = 6
-        # entry_5 = np.array(seq[0][5])
-        # entry_6 = np.array(seq[0][6])
-        entry_7 = 0
+        right_knee_angle = y_knee
+        left_knee_angle = y_knee
+        left_side_body_angle = y_body
+        right_side_body_angle = y_body
+        ankle_distance = 0.8
+        knee_distance = 1.25 - y_knee / 540
+        hip_angle = 0
 
         # Concatenate the entries to create the tensor
         squat_tensor = np.array([right_knee_angle, left_knee_angle, left_side_body_angle,
-                                 right_side_body_angle, entry_5, entry_6, entry_7])
+                                 right_side_body_angle, ankle_distance, knee_distance, hip_angle])
 
         # Append the tensor to the list
         data_list.append(squat_tensor)
@@ -44,12 +36,32 @@ def parabola(x):
 
 
 def deterministic_model_generation_function_parabola(amount_of_frames):
-    return _deterministic_model_generation_function(parabola, amount_of_frames)
+    return _deterministic_model_abstract_generation_function(parabola, parabola, amount_of_frames)
 
 
-def sin(x):
+def trigo_knees(x):
     return 180 - 90 * math.sin(math.pi * x)
 
 
-def deterministic_model_generation_function_sin(amount_of_frames):
-    return _deterministic_model_generation_function(sin, amount_of_frames)
+def trigo_body(x):
+    return 180 - 45 * math.sin(math.pi * x)
+
+
+def deterministic_model_generation_function_trigo(amount_of_frames):
+    return _deterministic_model_abstract_generation_function(trigo_knees, trigo_body, amount_of_frames)
+
+
+def deterministic_model_20(s):
+    return deterministic_model_generation_function_trigo(25)[-20:]
+
+
+def deterministic_model_15(s):
+    return deterministic_model_generation_function_trigo(25)[-15:]
+
+
+def deterministic_model_10(s):
+    return deterministic_model_generation_function_trigo(25)[-10:]
+
+
+def deterministic_model_5(s):
+    return deterministic_model_generation_function_trigo(25)[-5:]
--- experiments/run_experiment.py
+++ experiments/run_experiment.py
@@ -5,11 +5,28 @@ from infrastructure import measure_squat_performance
 from generation_functions import *
 from score_functions import *
 
-skeletons = np.ones(shape=(25, 7), dtype=float)
-data = deterministic_model_generation_function_sin(25)
-headers = ['right_knee_angle', 'left_knee_angle', 'left_side_body_angle', 'right_side_body_angle', 'ankle_distance',
-           'knee_distance', 'hip_angle']
-visualize_rep(data, headers)
-# s = measure_squat_performance(skeletons, score_function=mse, generation_function=test_15,
-#                               amount_of_skeletons_to_use_for_prediction=10)
-# print(s)
+reps = np.ones(shape=(20, 25, 7), dtype=float)
+# data = deterministic_model_generation_function_trigo(25)
+#
+# headers = ['right_knee_angle', 'left_knee_angle', 'left_side_body_angle', 'right_side_body_angle', 'ankle_distance',
+#            'knee_distance', 'hip_angle']
+# visualize_rep(data, headers)
+
+# squat_performance = measure_squat_performance(reps[1], score_function=mse, generation_function=deterministic_model_15,
+#                                               amount_of_skeletons_to_use_for_prediction=10)
+# print(squat_performance)
+
+amount_of_frames = 25
+squat_performances = []
+for i in range(5, amount_of_frames):
+    def det_model(rep):
+        return deterministic_model_generation_function_trigo(amount_of_frames)[-i:]
+
+
+    squat_performance = measure_squat_performance(reps[0], score_function=mse,
+                                                  generation_function=det_model,
+                                                  amount_of_skeletons_to_use_for_prediction=amount_of_frames - i)
+    squat_performances.append(squat_performance)
+
+squat_performances.reverse()
+print([np.mean(s) for s in squat_performances])
